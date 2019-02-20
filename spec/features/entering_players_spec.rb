882ce9e364989be435b44a017a9a187d4307483f
Steve Kahn <steve@kahn.org.uk>
2019-02-20 16:24:28 +0000

    Added test so that names are output that actually refer to the input

--- spec/features/entering_players_spec.rb
+++ spec/features/entering_players_spec.rb
@@ -6,4 +6,11 @@ feature "Entering players" do
     click_button( 'Good To Go!')
     expect(page).to have_content("Player One is Kate and Player Two is Steve")
   end
+  scenario "Player 1 is Simon and Player 2 is Luke" do
+    visit '/'
+    fill_in( 'Player One', with:'Simon')
+    fill_in( 'Player Two', with:'Luke')
+    click_button( 'Good To Go!')
+    expect(page).to have_content("Player One is Simon and Player Two is Luke")
+  end
 end
