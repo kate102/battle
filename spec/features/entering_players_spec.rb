4fcf4419ce484261d9cd555d41cbe65f0226bc95
Student <student@Admins-MacBook-Pro-4.local>
2019-02-21 11:34:46 +0000

    extracted the name printing from the name getting functionality and Fixed session bug

--- spec/features/entering_players_spec.rb
+++ spec/features/entering_players_spec.rb
@@ -3,14 +3,14 @@ feature "Entering players" do
     visit '/'
     fill_in( 'Player One', with:'Kate')
     fill_in( 'Player Two', with:'Steve')
-    click_button( 'Good To Go!')
+    click_button( 'Submit!')
     expect(page).to have_content("Player One is Kate and Player Two is Steve")
   end
   scenario "Player 1 is Simon and Player 2 is Luke" do
     visit '/'
     fill_in( 'Player One', with:'Simon')
     fill_in( 'Player Two', with:'Luke')
-    click_button( 'Good To Go!')
+    click_button( 'Submit!')
     expect(page).to have_content("Player One is Simon and Player Two is Luke")
   end
 end
